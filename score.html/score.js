a11e82a68318075c17091e5232f3129dc2075993
GabrielaBianca04 <iancub.gabriela@gmail.com>
2023-09-21 09:47:15 +0200

    score finale

---
+++ score.html/score.js
@@ -0,0 +1,31 @@
+/*let obj = document.getElementById("pie")
+
+if(obj>10% && obj<20%)
+{obj.setAttribute("style","transform:rotate(.1turn)")}
+
+if else(obj>20% && obj<30%)
+{obj.setAttribute("style","transform:rotate(.2turn)")}
+
+if else(obj>30% && obj<40%)
+{obj.setAttribute("style","transform:rotate(.3turn)")}
+
+if else(obj>40% && obj<50%)
+{obj.setAttribute("style","transform:rotate(.4turn)")}
+
+if else(obj>50% && obj<60%)
+{obj.setAttribute("style","transform:rotate(.5turn)")}
+
+if else(obj>60% && obj<70%)
+{obj.setAttribute("style","transform:rotate(.6turn)")}
+
+if else(obj>70% && obj<80%)
+{obj.setAttribute("style","transform:rotate(.7turn)")}
+
+if else(obj>80% && obj<90%)
+{obj.setAttribute("style","transform:rotate(.8turn)")}
+
+if else(obj>90% && obj<100%)
+{obj.setAttribute("style","transform:rotate(.9turn)")}
+
+else(obj=100%)
+{obj.setAttribute("style","transform:rotate(.turn)")}*/
